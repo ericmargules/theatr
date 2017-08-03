630c831495e993e5b89dc930224be6264d897828
Eric Margules <eric.i.margules@gmail.com>
2017-08-03 01:13:58 -0500

    add overall stylesheet and fix remaining admin routes

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,12 +1,32 @@
+function testInput(input) {
+	var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var cc_num = /^\d{16}$/;
+	var cc_exp = /^\d{4}$/;
+
+	switch(input[0].id){
+		case "order_email":
+			regexTest("#order_email", email);
+			break;
+		case "order_cc_num":
+			regexTest("#order_cc_num", cc_num);
+			break;
+		case "order_cc_exp":
+			regexTest("#order_cc_exp", cc_exp);
+			break;
+	}
+}
+
 function mark_error (input){
 	$(input)[0].parentElement.className = "field error";
 }
 
-function regex_test(input, regex){
+function regexTest(input, regex){
 	var test = true;
 	if($(input)[0].value.match(regex) == null){
 		$(input)[0].parentElement.className = "field error";
 		test = false;
+	}else{
+		$(input)[0].parentElement.className = "field";
 	};
 	return test;
 }
@@ -14,16 +34,15 @@ function regex_test(input, regex){
 function handleOrderForm() {
 	
 	$("#place_order").on("click", function() {
-		var test = true;
 		var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 		var cc_num = /^\d{16}$/;
 		var cc_exp = /^\d{4}$/;
 		
 
 		if(
-			!regex_test("#order_email", email) | 
-			!regex_test("#order_cc_num", cc_num) | 
-			!regex_test("#order_cc_exp", cc_exp)
+			!regexTest("#order_email", email) | 
+			!regexTest("#order_cc_num", cc_num) | 
+			!regexTest("#order_cc_exp", cc_exp)
 		){
 			return false;
 		};
@@ -33,4 +52,11 @@ function handleOrderForm() {
 
 $(document).ready(function(){
 	handleOrderForm();
+	
+	$(".required").on("blur", function() {
+        if ($(this)[0].parentElement.className == "field error"){
+			testInput($(this))
+		};
+	});
+
 });
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( order.js )
 Rails.application.config.assets.precompile += %w( order.css )
+Rails.application.config.assets.precompile += %w( theatr.css )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
