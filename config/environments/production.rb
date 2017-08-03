b543ce869edc0a1e801f3c6f7286cc8c509858a2
Eric Margules <eric.i.margules@gmail.com>
2017-08-02 23:09:47 -0500

    update heroku production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
-  config.action_mailer.default_url_options = { 'theatr.herokuapp.com' }
+  # config.action_mailer.default_url_options = { 'theatr.herokuapp.com' }
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
