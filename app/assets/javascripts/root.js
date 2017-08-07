fd0667d44c284db0fa4aad9e80ae0a5e030759ca
Eric Margules <eric.i.margules@gmail.com>
2017-08-06 23:04:49 -0500

    begin styling now playing bar

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,3 +1,35 @@
+function setSlides(){
+    var width = $(window).width();
+    var height = $(window).height();
+    var slideAmount;
+
+    if (width <= 799) {
+        slideAmount = 1;
+    } else if (width >= 800 && width <= 949) {
+        slideAmount = 2;
+    } else if (width >= 950 && width <= 1099) {
+        slideAmount = 3;
+    } else {
+        slideAmount = 4;
+    }
+    return slideAmount;
+}
+
+function checkCarousel(properties){
+    
+    if (properties.items != setSlides()) {
+        properties.items = setSlides()
+		$(".owl-carousel").trigger('destroy.owl.carousel');	    
+		startCarousel(properties);
+	}
+}
+
+function startCarousel(properties){
+	$(".owl-carousel").owlCarousel(properties);
+	$(".owl-carousel").trigger('play.owl.autoplay',[7000]);
+}
+
+
 function parseDay(link){
 	var npDivs = $(".showing_day");
 	npDivs.each(function(){
@@ -16,40 +48,19 @@ $(document).ready(function(){
 			return false;	
 	});
 
+	var slides = setSlides();
 	var properties = {
-	  	items: 4,
-	    loop:true,
-	    margin:0,
-	    nav:false,
-	    autoplay:true,
-	    autoplayTimeout:7000
+  	items: slides,
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:7000
   	};
-
-	$(".owl-carousel").owlCarousel(properties);
-
-	var owl = $('.owl-carousel');
-	owl.trigger('play.owl.autoplay',[7000]);
-
+ 	
+	startCarousel(properties);
 
 	$(window).resize(function() {
-	    var width = $(window).width();
-	    var height = $(window).height();
-	    var slideAmount;
-
-	    if (width <= 799) {
-	        slideAmount = 1;
-	    } else if (width >= 800 && width <= 899) {
-	        slideAmount = 2;
-	    } else if (width >= 900 && width <= 999) {
-	        slideAmount = 3;
-	    } else {
-	        slideAmount = 4;
-	    }
-
-	    if (properties.items != slideAmount) {
-	        properties.items = slideAmount
-			owl.trigger('destroy.owl.carousel');	    
-			$(".owl-carousel").owlCarousel(properties);
-		}
+		checkCarousel(properties);
 	});
 });
