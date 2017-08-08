e002ec62c5e9df8537b1237dc3627cf7aa7eeba4
Eric Margules <eric.i.margules@gmail.com>
2017-08-07 21:32:32 -0500

    style np showtimes and movie titles

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 	
 	devise_for :users
 	
+	devise_scope :user do
+		get '/admin' => 'devise/sessions#new'
+	end
+	
 	root 'welcome#index'
 
 	get 'user_root' => 'welcome#admin'
 
-	get '/admin' => 'welcome#admin'
 
 	resources :movies, :auditoriums, :orders, :showtimes
 
