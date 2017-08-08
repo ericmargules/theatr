a43eb8d138d2ce2773f87dee5ff2243785fc5626
Eric Margules <eric.i.margules@gmail.com>
2017-08-07 19:02:46 -0500

    style NP date select menu

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -32,13 +32,21 @@ function startCarousel(properties){
 
 function parseDay(link){
 	var npDivs = $(".showing_day");
+	var npDateSelect = $(".np_date_select");
+	npDateSelect.each(function (){
+		if($(this).hasClass("selected")){
+			$(this).toggleClass("selected");
+		};
+	});
+
 	npDivs.each(function(){
 		if($(this).hasClass("hidden") == false){
 			$(this).toggleClass("hidden");
 		};
 	});
 	var dayInt = parseInt(link.id[link.id.length-1]);
-	npDivs[dayInt].className = "showing_day"
+	npDivs[dayInt].className = "showing_day";
+	$(link.children[0]).toggleClass("selected");
 }
 
 $(document).ready(function(){
